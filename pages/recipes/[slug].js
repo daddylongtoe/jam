bc42a025d9439cc9f3da4814a38ecd2fa12257c9
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2021-06-18 14:54:24 +0100

    404

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
-import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Image from 'next/image';
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -18,7 +19,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -28,13 +29,24 @@ export async function getStaticProps({ params }) {
     'fields.slug': params.slug,
   });
 
+  if (!items.length) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   return {
-    props: { recipe: items[0] },
+    props: { recipe: items ? items[0] : null },
     revalidate: 1,
   };
 }
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
+
   const { featuredImage, cookingTime, title, ingredients, method } =
     recipe.fields;
 
