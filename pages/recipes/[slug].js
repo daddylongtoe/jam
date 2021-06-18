040e304271466d63e7f0cff220e8ae6ef1c14e52
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2021-06-18 11:47:06 +0100

    Revert "skeleton"
    This reverts commit c5744026345eadb83c5e0070962d21ad635498e7.

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,11 +1,10 @@
 import { createClient } from 'contentful';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Image from 'next/image';
-import Skeleton from '../../components/Skeleton';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  accessToken: process.env.CONTENTFUL_ACESS_KEY,
 });
 
 export const getStaticPaths = async () => {
@@ -13,19 +12,17 @@ export const getStaticPaths = async () => {
     content_type: 'recipe',
   });
 
-  const paths = res.items.map((item) => {
-    return {
-      params: { slug: item.fields.slug },
-    };
-  });
+  const paths = res.items.map((item) => ({
+    params: { slug: item.fields.slug },
+  }));
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
-export const getStaticProps = async ({ params }) => {
+export async function getStaticProps({ params }) {
   const { items } = await client.getEntries({
     content_type: 'recipe',
     'fields.slug': params.slug,
@@ -35,63 +32,56 @@ export const getStaticProps = async ({ params }) => {
     props: { recipe: items[0] },
     revalidate: 1,
   };
-};
+}
 
 export default function RecipeDetails({ recipe }) {
-  if (!recipe) return <Skeleton />;
-
-  const { featuredImage, title, cookingTime, ingredients, method } =
+  const { featuredImage, cookingTime, title, ingredients, method } =
     recipe.fields;
 
   return (
     <div>
       <div className="banner">
         <Image
-          src={'https:' + featuredImage.fields.file.url}
+          src={`https:${featuredImage.fields.file.url}`}
           width={featuredImage.fields.file.details.image.width}
           height={featuredImage.fields.file.details.image.height}
         />
         <h2>{title}</h2>
       </div>
-
       <div className="info">
-        <p>Takes about {cookingTime} mins to cook.</p>
-        <h3>Ingredients:</h3>
-
-        {ingredients.map((ing) => (
-          <span key={ing}>{ing}</span>
+        <p>Takes approx. {cookingTime} mins to cook</p>
+        <h3>Ingredients</h3>
+        {ingredients.map((ingredient) => (
+          <span key={ingredient}>{ingredient}</span>
         ))}
       </div>
-
       <div className="method">
         <h3>Method:</h3>
         <div>{documentToReactComponents(method)}</div>
       </div>
-
       <style jsx>{`
-        h2,
-        h3 {
+        h2,h3 {
           text-transform: uppercase;
         }
-        .banner h2 {
-          margin: 0;
+        .banner h2{
+          margin:0;
           background: #fff;
           display: inline-block;
           padding: 20px;
           position: relative;
           top: -60px;
           left: -10px;
-          transform: rotateZ(-1deg);
-          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          transform: rotateZ((-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
         }
-        .info p {
+        .info p{
           margin: 0;
         }
-        .info span::after {
-          content: ', ';
+        .info span::after{
+          content: ", ";
         }
-        .info span:last-child::after {
-          content: '.';
+        .info span:last-child::after{
+          content: ".";
         }
       `}</style>
     </div>
